f5ae08e0287a3f8cea2d237707ab18dd6ebd0c3b
Lahiru <lahirusampath.15@cse.mrt.ac.lk>
2024-03-14 21:47:09 +0530

    branch-1 - second commit

--- src/Application.java
+++ src/Application.java
@@ -2,5 +2,6 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("First");
         System.out.println("second - from branch 1");
+        System.out.println("third - from branch 1");
     }
 }
