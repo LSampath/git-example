e36025fd347f2b9a602f9439e836728a9081cce0
Lahiru <lahirusampath.15@cse.mrt.ac.lk>
2024-03-14 21:48:04 +0530

    master - third commit

--- src/Application.java
+++ src/Application.java
@@ -2,5 +2,6 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("First");
         System.out.println("second - from master");
+        System.out.println("third - from master");
     }
 }
