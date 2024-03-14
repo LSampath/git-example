ca91db1b4dc40cff5cd3894f6d821c2debb99a42
Lahiru <lahirusampath.15@cse.mrt.ac.lk>
2024-03-14 23:32:16 +0530

    branch-4 first commit

--- src/Application.java
+++ src/Application.java
@@ -14,5 +14,8 @@ public class Application {
 
         System.out.println("first - from branch-3");
         System.out.println("second - from branch-3");
+
+        // new branch
+        System.out.println("first - from branch 4");
     }
 }
